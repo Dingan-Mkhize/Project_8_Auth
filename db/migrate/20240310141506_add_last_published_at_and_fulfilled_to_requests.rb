1a0e64f5dcc94b7ac1f0d796c193ff909ae85fbd
Dingan Mkhize <dingan.mkhize@yahoo.com>
2024-03-12 18:10:21 +0000

    db/migrate/20240310141506_add_last_published_at_and_fulfilled_to_requests.rb unmigrated now migrated

--- db/migrate/20240310141506_add_last_published_at_and_fulfilled_to_requests.rb
+++ db/migrate/20240310141506_add_last_published_at_and_fulfilled_to_requests.rb
@@ -1,6 +1,7 @@
-class AddLastPublishedAtAndFulfilledToRequests < ActiveRecord::Migration[6.0]
+class AddLastPublishedAtAndFulfilledToRequests < ActiveRecord::Migration[7.0]
   def change
     add_column :requests, :last_published_at, :datetime
     add_column :requests, :fulfilled, :boolean, default: false
   end
 end
+
