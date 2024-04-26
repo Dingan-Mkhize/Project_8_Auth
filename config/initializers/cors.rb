196b13fa23dca956255893a36da3f570f6feeadc
Dingan Mkhize <dingan.mkhize@yahoo.com>
2024-04-26 15:05:04 +0100

    Update CORS settings for development

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://hands-united-react-302085c5109d.herokuapp.com'
+    origins '*' # This is for development only, to replace with app's URL for production.
 
     resource '*',
       headers: :any, 
