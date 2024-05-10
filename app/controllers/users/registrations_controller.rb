1875b200392a6770d131b71e972565c994e3fcca
Dingan Mkhize <dingan.mkhize@yahoo.com>
2024-05-10 14:47:24 +0100

    added array to fix government id error

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -67,7 +67,8 @@ end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :government_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation, { government_id: [] }
+  ])
 end
 
   # If you have extra params to permit, append them to the sanitizer.
